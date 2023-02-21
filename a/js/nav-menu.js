a0e4ab9a11a5c1b9e59cb912cfedb7f1ead9920a
yasar <yasarkursun99@gmail.com>
2023-02-21 14:10:39 +0300

    changes

---
+++ a/js/nav-menu.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector(".navbar-nav");
+const menuBtn = document.querySelector("#open-menu-btn")
+const closeBtn = document.querySelector("#close-menu-btn")
+
+// show/hide nav menu
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display= "none";
+})
+
+//close nav menu
+
+const closeNav = () => {
+    menu.style.display ="none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block"; 
+}
+
+closeBtn.addEventListener('click', closeNav)
